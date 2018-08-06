ee96b171e71f10979f9bb7127a3ed3e912344ef8
zachachilles <achilles@live.unc.edu>
2018-08-06 14:05:50 -0400

    Update EventManager.cs

--- EventManager.cs
+++ EventManager.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
-
+//I used this script to test how the VRTK levers work 
+//attach this script to the game object that has a VRTK lever attached 
 
 public class EventManager : MonoBehaviour {
     public delegate void LeverAction();
@@ -18,7 +19,7 @@ public class EventManager : MonoBehaviour {
     {
         bool triggered = false;
         VRTK_Lever l = child.GetComponent<VRTK_Lever>();
-        float num = 45;
+        float num = 45; //basically modify this value to test what numerical values correspond to an angle on the lever - it can be hard to tell what 45degrees is when you are twisting the levers around in 3d space basically
         if (l.CalculateValue() > num)
         {
             print("hello");
