8cab9b15d41a2f33756c08dd61e289f2a8027801
zachachilles <achilles@live.unc.edu>
2018-08-06 13:30:27 -0400

    Update CollisionPrinter.cs

--- CollisionPrinter.cs
+++ CollisionPrinter.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//if this script is attached to an object it prints the name of any objects that collide with it - however both objects must have a collider component attached and the object this is attached to must have the "ontriggerenter" option checked in the scene view under the options within the collider 
+//I used this to test how the collision system works and what the various collision dimensions actually mean in a practical sense
 public class CollisionPrinter : MonoBehaviour {
 
 	// Use this for initialization
