1f9b90c14921cc4900b9d982f9125401386ebadb
zachachilles <achilles@live.unc.edu>
2018-08-06 00:31:54 -0400

    Update Exiting.cs

--- Exiting.cs
+++ Exiting.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//this script exits the application if the escape key is pressed - basically ends the scene so the play button must be hit again
 public class Exiting : MonoBehaviour {
 
     void Update()
