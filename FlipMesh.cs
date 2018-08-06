53217246c8b66b8bf0f998d62ff04aff62e48cf6
zachachilles <achilles@live.unc.edu>
2018-08-06 00:33:55 -0400

    Update FlipMesh.cs

--- FlipMesh.cs
+++ FlipMesh.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-
+//certain meshes have a habit of being only generated on one side- they are invisible if looked at from the other side- this is a problem when objects can be picked up and rotated- this fixes that issue
 public class FlipMesh : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Mesh mesh = GetComponent<MeshFilter>().mesh;
-        mesh.triangles = mesh.triangles.Reverse().ToArray();
+        Mesh mesh = GetComponent<MeshFilter>().mesh; //the meshfilter component of the attached object is gotten
+        mesh.triangles = mesh.triangles.Reverse().ToArray(); //they are then reveresed via this line
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-    public void flip(GameObject objec)
+    public void flip(GameObject objec) //this does the same as above except in a more useful way- this class can be called with an object that needs to be rendered on two sides
     {
-        Mesh mesh = objec.GetComponent<MeshFilter>().mesh;
+        Mesh mesh = objec.GetComponent<MeshFilter>().mesh; 
         mesh.triangles = mesh.triangles.Reverse().ToArray();
     }
 }
