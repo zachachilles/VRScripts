98cc4e8f6afadd23a303f5ad7c29f57d16213522
zachachilles <achilles@live.unc.edu>
2018-08-06 13:27:20 -0400

    Update ButtonTest.cs

--- ButtonTest.cs
+++ ButtonTest.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//I used this script to test which buttons are which - in the update line you can add a command that calls the print() class if a button is pressed
 public class ButtonTest : MonoBehaviour {
 
 	// Use this for initialization
